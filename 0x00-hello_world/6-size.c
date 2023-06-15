bd28804f9b53661c295963fdf5c4e8a33dda9045
morris waithaka <morriskahiga62@gmail.com>
2023-06-15 13:34:35 -0700

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,55 +14,16 @@ int main(void)
 
 {
 
-        printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
 
-        printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
 
-        printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 
-        printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 
-        printf("Size of a float: %d byte(s)\n", sizeof(float));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 
-        return (0);
+return (0);
 
-}
-
-
-100-intel
-
-
-#!/bin/bash
-
-gcc -S -masm=intel $CFILE
-
-
-101-quote.c
-
-
-#include <stdio.h>
-
-#include <unistd.h>
-
-
-/**
-
-* main - Entry point
-
-*
-
-* Return: Always 1 (Success)
-
-*/
-
-int main(void)
-
-{
-
-        write(2,
-
-        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-        return (1);
-
-}
+}}
